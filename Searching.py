81f96c647c3a1d033d3dca1756993c6d9a380c08
Mark Umnus <merkrafter@gmail.com>
2019-11-03 17:29:16 +0100

    Add basic alpha-beta-pruning function

--- Searching.py
+++ Searching.py
@@ -30,3 +30,38 @@ class Node:
         :return: False (as this is a dummy implementation)
         """
         return False
+
+
+def alpha_beta_search(node, depth, alpha=-float('inf'), beta=float('inf'), maximising_player=True):
+    """
+    Calculates the game value from a given `node` searching at most `depth` levels utilizing a usual alpha beta pruning.
+    :param node: a `Node` instance
+    :param depth: how many levels to search
+    :param alpha:
+    :param beta:
+    :param maximising_player: True by default
+    :return: the value of the game until the current depth
+    """
+    # print("{} encountered node with value {} while alpha={}, beta={}".format(("min", "max")[maximising_player], node.item, alpha, beta))
+    if depth == 0 or node.is_terminal():
+        return node.value()
+    if maximising_player:
+        value = -float('inf')
+        for child in node.children():
+            value = max(value, alpha_beta_search(child, depth - 1, alpha, beta, not maximising_player))
+            alpha = max(alpha, value)
+            if alpha >= beta:
+                break
+        return value
+    else:  # maximising_player is False
+        value = float('inf')
+        for child in node.children():
+            value = min(value, alpha_beta_search(child, depth - 1, alpha, beta, not maximising_player))
+            beta = min(beta, value)
+            if alpha >= beta:
+                break
+        return value
+
+
+if __name__ == "__main__":
+    print(alpha_beta_search(Node(1), 3))
