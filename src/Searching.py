9631b2c1380251af1cd2c6fe7e40a2a270f12d68
Mark Umnus <merkrafter@gmail.com>
2019-11-10 13:12:44 +0100

    Fix alpha beta search algorithm

--- src/Searching.py
+++ src/Searching.py
@@ -46,24 +46,30 @@ def alpha_beta_search(node: Node, depth: int, alpha: float = -float('inf'), beta
     :param maximising_player: True by default
     :return: the value of the game until the current depth
     """
-    # print("{} encountered node with value {} while alpha={}, beta={}".format(("min", "max")[maximising_player], node.item, alpha, beta))
-    if depth == 0 or node.is_terminal():
+    if depth == 0:
         return node.value()
+    num_children = 0
     if maximising_player:
         value = -float('inf')
         for child in node.children():
+            num_children += 1
             value = max(value, alpha_beta_search(child, depth - 1, alpha, beta, not maximising_player))
             alpha = max(alpha, value)
             if alpha >= beta:
                 break
+        if num_children == 0:
+            return node.value()
         return value
     else:  # maximising_player is False
         value = float('inf')
         for child in node.children():
+            num_children += 1
             value = min(value, alpha_beta_search(child, depth - 1, alpha, beta, not maximising_player))
             beta = min(beta, value)
             if alpha >= beta:
                 break
+        if num_children == 0:
+            return node.value()
         return value
 
 
