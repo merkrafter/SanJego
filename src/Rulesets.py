8a20cbbd96269eb720120554422e483d867136b1
Mark Umnus <merkrafter@gmail.com>
2019-11-13 20:58:04 +0100

    Implement rule set for the base game #5

--- src/Rulesets.py
+++ src/Rulesets.py
@@ -18,10 +18,48 @@ class BaseRuleSet(object):
     def allows_move(self, from_pos: Tuple[int, int], to_pos: Tuple[int, int], player: int) -> bool:
         """
         Decides whether the given player is allowed to make the move given by the two positions on the board.
+        This basic implementation allows any two towers to be moved on top of each other if their positions meet either
+        vertically or horizontally (quad neighbourhood).
         :param from_pos: specifies the tower to move
         :param to_pos: specifies the tower to move on top of
         :param player: ID of a player that is registered in the `GameField` instance of this `RuleSet`
         :return: whether the player is allowed to make this move given this rule set
         """
-        pass
+        # perform basic checks
+        if not self.basically_allows_move(from_pos, to_pos):
+            return False
 
+        # check movement rule (quad neighbourhood) using manhattan distance
+        if abs(from_pos[0] - to_pos[0]) + abs(from_pos[1] - to_pos[1]) > 1:
+            return False
+
+        return True
+
+    def basically_allows_move(self, from_pos: Tuple[int, int], to_pos: Tuple[int, int]) -> bool:
+        """
+        This method performs basic checks on the move that likely apply to all rule sets.
+        In detail, it checks whether:
+         - both positions are not `None`
+         - both positions are different from each other
+         - both positions lie on the field
+         - there are towers on both positions
+        :param from_pos: specifies the tower to move
+        :param to_pos: specifies the tower to move on top of
+        :return: whether the above conditions are fulfilled by the given positions
+        """
+        # check whether both positions are not None
+        if from_pos is None or to_pos is None:
+            return False
+
+        # check whether both positions are different from each other
+        if from_pos == to_pos:
+            return False
+
+        # check whether both positions are on the board and
+        # check whether there are towers on both positions
+        top_tower = self.game_field.get_tower_at(from_pos)  # would return None if not on the board
+        lower_tower = self.game_field.get_tower_at(to_pos)
+        if top_tower is None or lower_tower is None or top_tower.height == 0 or lower_tower.height == 0:
+            return False
+
+        return True
