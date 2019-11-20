e8b58474669c4b74df569728096512540668cc57
Mark Umnus <merkrafter@gmail.com>
2019-11-20 21:12:34 +0100

    Fix: Make rules changes in run.py affect main.py

--- run.py
+++ run.py
@@ -6,10 +6,12 @@ MAX_AREA = 15
 
 rules = "base"
 
+
 sizes = [(h, w) for w in range(1, MAX_WIDTH) for h in range(1, w + 1) if h * w <= MAX_AREA]
 
 for height, width in sizes:
-    return_value = os.system(f"python main.py with height={height} width={width}")
+    command = f"python main.py with rules={rules} height={height} width={width}"
+    return_value = os.system(command)
     if return_value != 0:
         sys.stderr.write("previous calculation failed\n")
         break
@@ -17,7 +19,7 @@ for height, width in sizes:
     # due to symmetry etc. only fields with an odd number of cells need to be
     # recalculated for the min player beginning
     if (height * width) % 2 == 0:
-        return_value = os.system(f"python main.py with height={height} width={width} max_player_starts=False")
+        return_value = os.system(command + " max_player_starts=False")
         if return_value != 0:
             sys.stderr.write("previous calculation failed\n")
             break
